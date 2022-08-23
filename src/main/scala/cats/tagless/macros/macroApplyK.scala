c6e681855b9e4bb25afdc8277e20894a7fd2dbd4
Grigory Pomadchin <grigory.pomadchin@disneystreaming.com>
2022-08-23 02:43:35 -0400

    Add ApplyK

---
+++ src/main/scala/cats/tagless/macros/macroApplyK.scala
@@ -0,0 +1,33 @@
+package cats.tagless.macros
+
+import cats.tagless.*
+import quoted.*
+import cats.~>
+import cats.data.Tuple2K
+
+import scala.annotation.experimental
+import compiletime.asMatchable
+
+object macroApplyK {
+  import Utils.*
+
+  inline def derive[Alg[_[_]]] = ${ applyK[Alg] }
+
+  @experimental def applyK[Alg[_[_]]: Type](using Quotes): Expr[ApplyK[Alg]] = {
+    import quotes.reflect.*
+
+    val res = '{
+      new ApplyK[Alg] {
+        def mapK[F[_], G[_]](af: Alg[F])(fk: F ~> G): Alg[G] =
+          ${ macroFunctorK.capture('af, 'fk) }
+        def productK[F[_], G[_]](af: Alg[F], ag: Alg[G]): Alg[Tuple2K[F, G, *]] =
+          ${ macroSemigroupalK.capture('af, 'ag) }
+      }
+    }
+
+    // println("-----------")
+    // println(res.show)
+    // println("-----------")
+    res
+  }
+}
