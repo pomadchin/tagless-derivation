d4a443a8bdb53905191a0f344ad57389916fd7f3
Grigory Pomadchin <grigory.pomadchin@disneystreaming.com>
2022-08-23 09:20:34 -0400

    Add a sinlge Derive entry point

--- src/test/scala/cats/tagless/SemigroupalKSpec.scala
+++ src/test/scala/cats/tagless/SemigroupalKSpec.scala
@@ -3,26 +3,26 @@ package cats.tagless
 import cats.Id
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
-import cats.tagless.macros.*
+import cats.tagless.macros.Derive
 import cats.arrow.FunctionK
 import cats.data.Tuple2K
 
 class SemigroupalKSpec extends AnyFunSpec with Matchers with Fixtures:
   describe("SemigorupalK Spec") {
     it("DeriveMacro should derive instance for a simple algebra") {
-      val semigroupalK = macroSemigroupalK.derive[SimpleService]
+      val semigroupalK = Derive.semigroupalK[SimpleService]
       semigroupalK `shouldBe` a[SemigroupalK[SimpleService]]
     }
 
     it("SemigorupalK should be a valid instance for a simple algebra") {
-      val functorK         = macroFunctorK.derive[SimpleService]
-      val semigroupalK     = macroSemigroupalK.derive[SimpleService]
+      val functorK         = Derive.functorK[SimpleService]
+      val semigroupalK     = Derive.semigroupalK[SimpleService]
       val optionalInstance = functorK.mapK(instance)(FunctionK.lift([X] => (id: Id[X]) => Some(id)))
       val combinedInstance = semigroupalK.productK(instance, optionalInstance)
 
       combinedInstance.id() `shouldBe` Tuple2K(instance.id(), optionalInstance.id())
       combinedInstance.list(0) `shouldBe` Tuple2K(instance.list(0), optionalInstance.list(0))
       combinedInstance.paranthesless `shouldBe` Tuple2K(instance.paranthesless, optionalInstance.paranthesless)
-      combinedInstance.tuples `shouldBe` Tuple2K(instance.tuples, optionalInstance.tuples)
+      combinedInstance.tuple `shouldBe` Tuple2K(instance.tuple, optionalInstance.tuple)
     }
   }
